2da2df0f3e377992e23363d3423fb4ec07f1ad12
Marek Melichar <marek.melichar@icloud.com>
2021-03-27 21:09:52 +0100

    cleanup the start

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,35 +6,28 @@ const path = require("path");
 const PurgeCSSPlugin = require("purgecss-webpack-plugin");
 
 module.exports = merge(common, {
-	mode: "production",
-	module: {
-		rules: [
-			// {
-			//   test: /\.js$/,
-			//   exclude: /node_modules/,
-			//   use: {
-			//     loader: "babel-loader",
-			//   },
-			// },
-			{
-				test: /\.s[ac]ss$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					"css-loader",
-					"postcss-loader",
-					"sass-loader",
-				],
-			},
-		],
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "[name].[chunkhash:8].css",
-			chunkFilename: "[id].css",
-		}),
-		new PurgeCSSPlugin({
-			paths: glob.sync(path.resolve(__dirname, "./*.html")),
-			minimize: true,
-		}),
-	],
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash:8].css",
+      chunkFilename: "[id].css",
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync(path.resolve(__dirname, "./*.html")),
+      minimize: true,
+    }),
+  ],
 });
